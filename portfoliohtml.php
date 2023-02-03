636414a29b781dff76085d5bcb57d5d45b21956c
khushi muhammad <84716396+khushi8686@users.noreply.github.com>
2023-02-04 00:32:16 +0500

    first commit

---
+++ portfoliohtml.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Khushi Muhammad's Portfolio</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+body {
+background: linear-gradient(to bottom, #F9B872, #FAE7A5);
+}
+
+.header {
+background: rgba(249, 184, 114, 0.8);
+color: #111439;
+padding: 20px;
+text-align: center;
+box-shadow: 0px 5px 10px rgba(250, 231, 165, 0.8);
+animation: headerAnim 1s;
+border-radius: 10px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+}
+
+.header a {
+display: inline-block;
+padding: 10px 20px;
+text-decoration: none;
+color: #111439;
+margin: 0 10px;
+font-weight: bold;
+}
+
+h1 {
+font-size: 36px;
+font-weight: bold;
+text-transform: uppercase;
+margin-bottom: 20px;
+animation: titleAnim 1s;
+}
+
+p {
+font-size: 20px;
+line-height: 1.5;
+margin-bottom: 20px;
+animation: paragraphAnim 1s;
+}
+
+.about {
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #B6E1E7;
+border-radius: 10px;
+}
+
+.about .picture {
+width: 250px;
+height: 250px;
+border-radius: 50%;
+overflow: hidden;
+margin-right: 20px;
+animation: pictureAnim 1s;
+box-shadow: 0px 5px 10px rgba(250, 231, 165, 0.8);
+}
+
+.about .picture img {
+width: 100%;
+height: 100%;
+object-fit: cover;
+}
+
+.about .info {
+width: 50%;
+background: rgba(249, 184, 114, 0.8);
+color: #111439;
+padding: 20px;
+box-shadow: 0px 5px 10px rgba(250, 231, 165, 0.8);
+border-radius: 10px;
+}
+
+.about h2 {
+font-size: 36px;
+font-weight: bold;
+margin-bottom: 20px;
+animation: infoTitleAnim 1s;
+}
+
+.about p {
+font-size: 20px;
+line-height: 1.5;
+text-align: justify;
+animation: infoParagraphAnim 1s;
+}
+
+.contact {
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #B6E1E7;
+border-radius: 10px;
+}
+
+.contact .info {
+width: 50
+
+</style>
+</head>
+<body>
+  <div class="header">
+    <h1>Khushi Muhammad's Portfolio</h1>
+  </div>
+  <div class="about">
+    <div class="picture">
+      <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/events/IMG_20211216_182332.jpg" alt="Khushi Muhammad's Picture">
+    </div>
+    <div class="info">
+      <h2>About Me</h2>
+      <p>Hey I am Khushi Muhammad, currently pursuing a degree in computer science at Air University. During the past year, I have been involved in numerous projects and hackathons, as well as serving as a technical lead for our university. When I started programming 5 years ago when I was 17 years old, I learned many programming languages such as C,C# and C++ ,JAVA, Phython,KotLine,Dart and php.Currently I work on a military project based on the Dot Net Framework with Azur Cloud. </p>
+    </div>
+  </div>
+</body>
+</html>
+
+
